819b45c7defaa5a5b7b9552904e1d9bfee61fc81
Jesse Stowijk <stolwijkjesse@gmail.com>
2018-12-01 15:18:43 +0100

    cleanup, parse string to i32

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,15 @@ fn main() {
     let file = File::open(filename).expect("file not found");
 
     let reader = BufReader::new(file);
-    let lines = reader.lines().map(|line| line.unwrap()).collect();
+    let lines = reader
+        .lines()
+        .map(|line| i32::from_str(&line.unwrap()).expect("error parsing string to number"))
+        .collect();
 
     println!("{}", calc_repeated_frequency(lines));
 }
 
-fn calc_repeated_frequency(changes: Vec<String>) -> i32 {
+fn calc_repeated_frequency(changes: Vec<i32>) -> i32 {
     let max_index = changes.len() - 1;
 
     let mut set = HashSet::<i32>::new();
@@ -43,19 +46,12 @@ fn calc_repeated_frequency(changes: Vec<String>) -> i32 {
     frequency
 }
 
-fn calc_frequency(changes: Vec<String>) -> i32 {
+fn calc_frequency(changes: Vec<i32>) -> i32 {
     changes.iter().fold(0, calc_next_frequency)
 }
 
-fn calc_next_frequency(acc: i32, item: &String) -> i32 {
-    let operator = &item[0..1];
-    let value = &item[1..item.len()];
-    let number = i32::from_str(value).expect("error parsing string to number");
-    match operator {
-        "+" => acc + number,
-        "-" => acc - number,
-        _ => acc,
-    }
+fn calc_next_frequency(acc: i32, item: &i32) -> i32 {
+    acc + item
 }
 
 #[cfg(test)]
@@ -64,46 +60,28 @@ mod tests {
 
     #[test]
     fn calc_frequency_test() {
-        let input = vec!["+1".to_string(), "+1".to_string(), "+1".to_string()];
+        let input = vec![1, 1, 1];
         assert_eq!(3, calc_frequency(input));
 
-        let input = vec!["+1".to_string(), "+1".to_string(), "-2".to_string()];
+        let input = vec![1, 1, -2];
         assert_eq!(0, calc_frequency(input));
 
-        let input = vec!["-1".to_string(), "-2".to_string(), "-3".to_string()];
+        let input = vec![-1, -2, -3];
         assert_eq!(-6, calc_frequency(input));
     }
 
     #[test]
     fn calc_repeated_frequency_test() {
-        let input = vec!["+1".to_string(), "-1".to_string()];
+        let input = vec![1, -1];
         assert_eq!(0, calc_repeated_frequency(input));
 
-        let input = vec![
-            "+3".to_string(),
-            "+3".to_string(),
-            "+4".to_string(),
-            "-2".to_string(),
-            "-4".to_string(),
-        ];
+        let input = vec![3, 3, 4, -2, -4];
         assert_eq!(10, calc_repeated_frequency(input));
 
-        let input = vec![
-            "-6".to_string(),
-            "+3".to_string(),
-            "+8".to_string(),
-            "+5".to_string(),
-            "-6".to_string(),
-        ];
+        let input = vec![-6, 3, 8, 5, -6];
         assert_eq!(5, calc_repeated_frequency(input));
 
-        let input = vec![
-            "+7".to_string(),
-            "+7".to_string(),
-            "-2".to_string(),
-            "-7".to_string(),
-            "-4".to_string(),
-        ];
+        let input = vec![7, 7, -2, -7, -4];
         assert_eq!(14, calc_repeated_frequency(input));
     }
 }
